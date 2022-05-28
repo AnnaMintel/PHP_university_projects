00810786edf4d16b0becd967e4f38401f26dc104
AnnaMintel <annikamintel99@yandex.ru>
2022-05-28 16:55:06 +0300

    init: created task for finding simple integer and print it in table

---
+++ integersFirstTask.php
@@ -0,0 +1,60 @@
+<html>
+ <head>
+  <title>task1</title>
+ </head>
+ <body>
+
+ <?php echo '<h2>Find 10 first integers and print it in table </h2>'; ?>
+
+<?php
+$arr = [];
+$x = 0;
+
+for ($i = 2; $x < 10; $i++) {
+    $flag = 1;
+    if ($i > 2 && $i % 2 != 0) {
+        for ($j = 3; $j * $j <= $i; $j = $j + 2) {
+            if ($i % $j == 0) {
+                $flag = 0;
+                break;
+            }
+        }
+    } else if ($i != 2) $flag = 0;
+    if ($flag == 1) {
+        $arr[] = $i;
+        $x++;
+    }
+}               
+
+
+$rows = 10; 
+$cols = 2; 
+
+echo '<table border="1">';
+
+foreach ($arr as $key => $value) {
+    echo '<tr>';
+    echo '<td>'. $key + 1 .'</td>';
+    echo '<td>'. $value .'</td>';
+    echo '</tr>';
+}
+
+// for ($tr=1; $tr<=$rows; $tr++){ 
+//     echo '<tr>';
+//     foreach ($arr as &$value) {
+//         echo '<td>'. $value .'</td>';
+//     }
+//     // for ($td=1; $td<=$cols; $td++){ 
+//     //     echo '<td>'. $tr*$td .'</td>';
+//     // }
+//     echo '</tr>';
+// }
+echo '</table>';
+
+
+
+?>
+
+
+ </body>
+</html>
